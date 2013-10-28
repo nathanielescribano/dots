12f6ae5337cac552659b8d80fe624f899af6e981
Nathaniel Escribano <nescribano@blurb.com>
2013-10-28 13:47:53 -0700

    added an irbrc file to allow me to edit in vi

---
+++ .irbrc
@@ -0,0 +1,6 @@
+# you may have to run
+# gem install interactive_editor
+require 'rubygems'
+require 'interactive_editor'
+
+# this lets you type vi<enter> in an irb session
--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ rm ~/.pentadactylrc
 rm -r ~/.pentadactyl
 rm ~/.tmux.conf
 rm ~/.zlogin
+rm ~/.irbrc
 rm ~/.zlogout
 rm ~/.zpreztorc
 rm -r ~/.zprezto
@@ -21,6 +22,7 @@ ln -s ~/.dots/.tmux.conf ~/.tmux.conf
 ln -s ~/.dots/.screenrc ~/.screenrc
 ln -s ~/.dots/.vimrc ~/.vimrc
 ln -s ~/.dots/.vim ~/.vim
+ln -s ~/.dots/.irbrc ~/.irbrc
 
 # cd ~/.dots/.vim/bundle/vim-commandt
 # rake make
